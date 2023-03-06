638563c75dfef16b576367e550a3ad9ee66ae5a9
LamaPirat <g.skrefsrud@gmail.comg.skrefsrud@gmail.com>
2023-03-06 12:20:19 +0100

    mobile menu goodnuff

--- mobile-menu/script.js
+++ mobile-menu/script.js
@@ -7,6 +7,42 @@ let donuts = [
   },
 ];
 
+let pizzas = [
+  {
+    price: "$32",
+    imageSrc: "./recourses/pizza1.png",
+    name: "Pizza mayham",
+    origin: "Dolly dimples",
+  },
+];
+
+let burgers = [
+  {
+    price: "$32",
+    imageSrc: "./recourses/burger1.png",
+    name: "Stacks of heaven",
+    origin: "Burger Masters",
+  },
+];
+
+let milkshakes = [
+  {
+    price: "$32",
+    imageSrc: "./recourses/milkshake1.png",
+    name: "Milky dream",
+    origin: "Dairy King",
+  },
+];
+
+let pancakes = [
+  {
+    price: "$32",
+    imageSrc: "./recourses/pancakes1.png",
+    name: "Breakfast delight",
+    origin: "Sweet n sweeter",
+  },
+];
+
 document.addEventListener("click", (e) => {
   const isSelector = e.target.matches("[data-selector]");
 
@@ -41,11 +77,70 @@ function renderItem(price, imageSrc, name, origin) {
   document.querySelector(".selected-display").appendChild(itemElement);
 }
 
-for (let i = 0; i < 4; i++) {
-  renderItem(
-    donuts[0].price,
-    donuts[0].imageSrc,
-    donuts[0].name,
-    donuts[0].origin
-  );
+function clearDisplay() {
+  let display = document.querySelector(".selected-display");
+
+  while (display.firstChild) {
+    display.removeChild(display.firstChild);
+  }
+}
+
+function renderDonuts() {
+  clearDisplay();
+  for (let i = 0; i < 4; i++) {
+    renderItem(
+      donuts[0].price,
+      donuts[0].imageSrc,
+      donuts[0].name,
+      donuts[0].origin
+    );
+  }
+}
+
+function renderPizzas() {
+  clearDisplay();
+  for (let i = 0; i < 4; i++) {
+    renderItem(
+      pizzas[0].price,
+      pizzas[0].imageSrc,
+      pizzas[0].name,
+      pizzas[0].origin
+    );
+  }
+}
+
+function renderBurgers() {
+  clearDisplay();
+  for (let i = 0; i < 4; i++) {
+    renderItem(
+      burgers[0].price,
+      burgers[0].imageSrc,
+      burgers[0].name,
+      burgers[0].origin
+    );
+  }
+}
+
+function renderMilkshakes() {
+  clearDisplay();
+  for (let i = 0; i < 4; i++) {
+    renderItem(
+      milkshakes[0].price,
+      milkshakes[0].imageSrc,
+      milkshakes[0].name,
+      milkshakes[0].origin
+    );
+  }
+}
+
+function renderPancakes() {
+  clearDisplay();
+  for (let i = 0; i < 4; i++) {
+    renderItem(
+      pancakes[0].price,
+      pancakes[0].imageSrc,
+      pancakes[0].name,
+      pancakes[0].origin
+    );
+  }
 }
